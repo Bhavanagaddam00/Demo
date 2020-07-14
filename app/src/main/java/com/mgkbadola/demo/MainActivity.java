0803e497eed895efbc53771395fbca9f098a9590
hp <bhavana.gaddam00@gmail.com>
2020-07-14 14:34:44 +0530

    This project is about Button Animations.

---
+++ app/src/main/java/com/mgkbadola/demo/MainActivity.java
@@ -0,0 +1,39 @@
+package com.mgkbadola.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFab = findViewById(R.id.floatingActionButton);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presentActivity(v);
+            }
+        });
+    }
+
+    public void presentActivity(View view) {
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(this, view, "transition");
+        int revealX = (int) (view.getX() + view.getWidth() / 2);
+        int revealY = (int) (view.getY() + view.getHeight() / 2);
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(SecondActivity.EXTRA_CIRCULAR_REVEAL_X, revealX);
+        intent.putExtra(SecondActivity.EXTRA_CIRCULAR_REVEAL_Y, revealY);
+        ActivityCompat.startActivity(this, intent, options.toBundle());
+    }
+}
+
